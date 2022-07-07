31bb44a70a7ca9b650f17f506fb6fb0231f35a27
Remdec <remydecocq@gmail.com>
2022-07-07 16:30:07 +0200

    Detection script

--- packager.py
+++ packager.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from pathlib import Path
+from uninstaller_list import UNINSTALLERS
 
 dir_to_package = Path("./ToPackage")
 dir_to_package.mkdir(exist_ok=True)
@@ -20,21 +21,32 @@ def install_cmd(software_name, installer_type):
     if installer_type == ".exe":
         return '"%%~dp0sources\%s%s" /s' % (software_name, installer_type)
     elif installer_type == ".msi":
-        return 'MsiExec.exe "sources\%s%s" /qn' % (software_name, installer_type)
+        return 'MsiExec.exe /i "sources\%s%s" /qn' % (software_name, installer_type)
 
 def uninstall_cmd(software_name, installer_type):
+    uninstall_info = UNINSTALLERS.get(software_name)
     if installer_type == ".exe":
-        return '"C:\Program Files (x86)\%s\\uninst.exe" /s' % software_name
+        if uninstall_info:
+            return r'"%s" /S' % uninstall_info
+        return r'"C:\Program Files (x86)\%s\uninst.exe" /S' % software_name
     elif installer_type == ".msi":
+        if uninstall_info:
+            return r'MsiExec /x "{%s}" /qn' % uninstall_info
         return 'MsiExec /x "{GUID}" /qn'
+
+def detect_cmd(software_name):
+    uninstaller = UNINSTALLERS.get(software_name)
+    if uninstaller:
+        return r'if ((Test-Path -Path "%s")) { exit 0 } else { exit 1 }' % uninstaller
+    return None
+
 
 def package_cmd(dir_package, installer_target):
     return 'IntuneWinAppUtil.exe -c "%s" -s "%s" -o Packaged' % (dir_package, installer_target)
 
 def prepare_packages(installers):
-    print(installers)
     for installer in installers:
-        print("Found installer to treat : ", installer)
+        print("\nFound installer to treat : ", installer)
         software_name = installer.stem
         installer_type = installer.suffix
         # create directory structure
@@ -44,7 +56,6 @@ def prepare_packages(installers):
         dir_src.mkdir(exist_ok=True)
         # move the software installer to a fixed relative path
         installer_target = dir_src / (software_name + installer_type)
-        print(installer_target)
         installer.replace(installer_target)
         # create and fill in (un)install scripts with default commands
         install_script = dir_package / "Install.CMD"
@@ -54,7 +65,11 @@ def prepare_packages(installers):
         # create the snippet to launch later after finetuning to package towards .intunewin
         package_script = Path("PACK_" + software_name + ".CMD")
         package_script.write_text(package_cmd(dir_package, installer_target))
-
+        # create the detection script to provide to InTune
+        detect_command = detect_cmd(software_name)
+        if detect_command:
+            detection_script = Path(dir_packaged / ("DETECT_" + software_name + ".ps1"))
+            detection_script.write_text(detect_command)
 
 
 if __name__ == '__main__':
---
+++ uninstaller_list.py
@@ -0,0 +1,10 @@
+UNINSTALLERS = {
+    "7-Zip": r'C:\Program Files\7-Zip\Uninstall.exe',
+    "Belgium Identity Card": r'',
+    "Git": r'C:\Program Files\Git\unins000.exe',
+    "KeePass": r'C:\Program Files\KeePass\unins000.exe',
+    "MOBIB_Reader": None,
+    "Notepad++": r'C:\Program Files\Notepad++\uninstall.exe',
+    "Phish Alert": r'F3E990F3-6238-4294-97E6-DD252C1F2BCB',
+    "Postman": None
+}
